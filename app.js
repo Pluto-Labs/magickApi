84aef457e1a285ae6f5b47a750645ba65c26e4a4
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2021-08-18 21:48:25 -0300

    wip

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { exec } = require("child_process")
 const app = express()
 const port = 3000
 
@@ -11,7 +12,19 @@ app.get('/convertido', (req, res) => {
 })
 
 app.get('/convert', (req, res) => {
-  res.status(200).sendFile(__dirname + '/src/img/teste.jpg')
+  exec("ls -la", (error, stdout, stderr) => {
+    if (error) {
+      res.status(500).send(`error: ${error.message}`)
+      return
+    }
+
+    if (stderr) {
+      res.status(200).send(`stderr: ${stderr}`)
+      return
+    }
+
+    res.status(200).send(`stdout: ${stdout}`)
+  })
 })
 
 app.listen(port, () => {
