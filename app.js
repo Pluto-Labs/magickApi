18f083bfc4f07a19e748705b86f7c3d87c1b5f6e
Tales Peres <tales.oliveira@alunos.fho.edu.br>
2021-08-21 02:05:35 -0300

    wip

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const downloadImage = (imageUrl, imageName, res) => {
     file.on('finish', function () {
       file.close()
 
-      exec(`convert src/img/${imageName} -liquid-rescale 50x100%! -resize 110% src/img/${imageName}`, (error, stdout, stderr) => {
+      exec(`convert src/img/${imageName} -liquid-rescale 100x100%! -resize 110% src/img/${imageName}`, (error, stdout, stderr) => {
         if (error) {
           console.error(`error: ${error.message}`)
         } else {
